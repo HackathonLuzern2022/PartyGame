f4dd8bf9166f205907f458a0d31d30ed3b5791b0
Rood <48691039+Babaloopey@users.noreply.github.com>
2022-10-28 17:50:38 +0200

    Added fixture

---
+++ backend/routes/fixture.js
@@ -0,0 +1,11 @@
+const Router = require('express-promise-router')
+const router = new Router()
+
+const db = require('../db')
+
+module.exports = router
+
+router.get('/fixture', async(req, res) => {
+    const { rows } = await db.query('SELECT * FROM TASKS')
+    res.send(JSON.stringify(rows))
+})
