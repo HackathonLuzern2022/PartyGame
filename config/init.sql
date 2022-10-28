6223a1be8e2ed4c86e8a353e4aa2883704b70d50
Rood <48691039+Babaloopey@users.noreply.github.com>
2022-10-28 18:20:59 +0200

    added fixture-data

--- config/init.sql
+++ config/init.sql
@@ -3,3 +3,30 @@ create table tasks (
     hardness varchar(20),
     text varchar(255)
 );
+
+
+INSERT INTO TASKS (id, hardness, text) VALUES (1, 'EINFACH', '(Name) Erzähle deine peinlichste geschichte.'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (2, 'EINFACH', '(Name) zeige deine letzte versendete oder bekommene sprachnachricht'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (3, 'EINFACH', '(Name) zeige dein zuletzt gelöstes foto'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (4, 'EINFACH', '(Name) schweige für die nächste runde'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (5, 'EINFACH', '(Name) sprich für die nächste runde mit einem akzent'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (6, 'EINFACH', '(Name) deine mitspieler mischen dir einen mystery shot'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (7, 'EINFACH', '(Name) tausche dein getränk mit dem vollsten am tisch (ausser es ist dein eigenes)'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (8, 'EINFACH', '(Name) sing ein lied'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (9, 'EINFACH', '(Name) du darfst eine neue regel für diese runde festlegen'); 
+INSERT INTO TASKS (id, hardness, text) VALUES (10, 'EINFACH', '(Name) mache 10 liegestützen');
+
+ 
+INSERT INTO TASKS (id, hardness, text) VALUES (11, 'MITTEL', '(Name) trink dein bodycount');
+INSERT INTO TASKS (id, hardness, text) VALUES (12, 'MITTEL', '(Name) tanze zu einem song deiner wahl für 1 minute');
+INSERT INTO TASKS (id, hardness, text) VALUES (13, 'MITTEL', '(Name) rufe den 3. kontakt in deinem handy an und singe happy birthday');
+INSERT INTO TASKS (id, hardness, text) VALUES (14, 'MITTEL', '(Name) versuche deine beine hinter den kopf zu bekommen');
+INSERT INTO TASKS (id, hardness, text) VALUES (15, 'MITTEL', '(Name) imitier ein tier');
+INSERT INTO TASKS (id, hardness, text) VALUES (15, 'MITTEL', '(Name) mach einen purzelbaum');
+INSERT INTO TASKS (id, hardness, text) VALUES (15, 'MITTEL', '(Name) iss etwas ohne die hände zur hilfe zu nehmen');
+
+INSERT INTO TASKS (id, hardness, text) VALUES (16, 'SCHWIERIG', '(Name) lecke den boden ab');
+INSERT INTO TASKS (id, hardness, text) VALUES (17, 'SCHWIERIG', '(Name) geh nach hause');
+INSERT INTO TASKS (id, hardness, text) VALUES (18, 'SCHWIERIG', '(Name) die person die dich am besten kennt darf dir eine aufgabe stellen');
+INSERT INTO TASKS (id, hardness, text) VALUES (19, 'SCHWIERIG', '(Name) summ ein lied, wenn es niemand errät musst du die bestrafung trotzdem machen');
+INSERT INTO TASKS (id, hardness, text) VALUES (20, 'SCHWIERIG', '(Name) führe eine neue regel fürs ganze spiel ein');
