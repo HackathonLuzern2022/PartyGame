810ce3a385d81d0c62fad836f5aa3aa28da772f8
3n3a <info@3n3a.ch>
2022-10-28 15:02:05 +0200

    add initi

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const index = require('./routes/index')
+
+app.use('/', index)
+
+
+app.listen(port, () => {
+    console.log(`Party Game BE listening on port ${port}`)
+})
